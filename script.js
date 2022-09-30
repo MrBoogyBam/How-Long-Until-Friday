1bcc700a9e695edfcac25656a599a6f873f78d6a
MrBusinessMan <mrboogybambusiness@gmail.com>
2022-09-30 04:02:00 +0300

    made it so timer doesn't go into negatives

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 let timer = document.getElementById("timer");
+let video = document.getElementById("week-guy");
 
 let now;
 let nextFridate;
+let msTillFriday;
 
 let timerFunc = setInterval(function() {
     now = new Date();
     nextFridate = new Date(now.getUTCFullYear().toString() + "-" + (now.getUTCMonth() + 1) + "-" + (now.getDate() - now.getDay() + 5));
-    let msTillFriday = nextFridate.getTime() - now.getTime();
+    msTillFriday = nextFridate.getTime() - now.getTime();
+
+    if(msTillFriday <= 0) {
+        msTillFriday = 0;
+    }
 
     let days = Math.floor(msTillFriday / (1000 * 60 * 60 * 24));
     let hours = Math.floor((msTillFriday % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
