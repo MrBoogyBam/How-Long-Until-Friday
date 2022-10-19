9bb297eed07a6c8ffa9360aa7f93a2b0b6a9d590
MrBusinessMan <mrboogybambusiness@gmail.com>
2022-10-19 15:12:18 +0300

    Friday²

--- script.js
+++ script.js
@@ -4,20 +4,23 @@ let video = document.getElementById("week-guy");
 let now;
 let nextFridate;
 let msTillFriday;
+let msTillFridayStr;
 
 let timerFunc = setInterval(function() {
-    now = new Date();
-    nextFridate = new Date(now.getUTCFullYear().toString() + "-" + (now.getUTCMonth() + 1) + "-" + (now.getDate() - now.getDay() + 5));
-    msTillFriday = nextFridate.getTime() - now.getTime();
+  now = new Date();
+  nextFridate = new Date(now.getUTCFullYear().toString() + "-" + (now.getUTCMonth() + 1) + "-" + (now.getDate() - now.getDay() + 5));
+  msTillFriday = nextFridate.getTime() - now.getTime();
+  msTillFridayStr = msTillFriday.toString();
 
-    if(msTillFriday <= 0) {
-        msTillFriday = 0;
-    }
+  if (msTillFriday <= 0) {
+    msTillFriday = 0;
+  }
 
-    let days = Math.floor(msTillFriday / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((msTillFriday % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((msTillFriday % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((msTillFriday % (1000 * 60)) / 1000);
+  let days = ('0' + Math.floor(msTillFriday / (1000 * 60 * 60 * 24))).slice(-2);
+  let hours = ('0' + Math.floor((msTillFriday % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).slice(-2);
+  let minutes = ('0' + Math.floor((msTillFriday % (1000 * 60 * 60)) / (1000 * 60))).slice(-2);
+  let seconds = ('0' + Math.floor((msTillFriday % (1000 * 60)) / 1000)).slice(-2);
+  let milliseconds = msTillFridayStr.slice(msTillFridayStr.length - 3);
 
-    timer.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+  timer.innerHTML = `${days}:${hours}:${minutes}:${seconds}.${milliseconds}`;
 }, 1);
